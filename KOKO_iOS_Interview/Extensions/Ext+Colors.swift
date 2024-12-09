51bd4d8dcb48730f2ceb152bd21f5f6d59010050
WeiFangChou <chou.wei.fang@gmail.com>
2024-12-09 14:28:39 +0800

    Add Extensions Colors

--- KOKO_iOS_Interview/Extensions/Ext+Colors.swift
+++ KOKO_iOS_Interview/Extensions/Ext+Colors.swift
@@ -1,7 +1,25 @@
 //
-//  Colors.swift
+//  UIColors.swift
 //  KOKO_iOS_Interview
 //
 //  Created by WeiFangChou on 2024/12/9.
 //
 
+import UIKit
+
+extension UIColor {
+	static let appleGreen = UIColor(red: 121.0 / 255.0, green: 196.0 / 255.0, blue: 27.0 / 255.0, alpha: 1.0)
+	static let lightGrey = UIColor(white: 153.0 / 255.0, alpha: 1.0)
+	static let steel = UIColor(red: 142.0 / 255.0, green: 142.0 / 255.0, blue: 147.0 / 255.0, alpha: 1.0)
+	static let whiteThree = UIColor(white: 228.0 / 255.0, alpha: 1.0)
+	static let appleGreen40 = UIColor(red: 121.0 / 255.0, green: 196.0 / 255.0, blue: 27.0 / 255.0, alpha: 0.4)
+	static let greyishBrown = UIColor(white: 71.0 / 255.0, alpha: 1.0)
+	static let pinkishGrey = UIColor(white: 201.0 / 255.0, alpha: 1.0)
+	static let booger = UIColor(red: 166.0 / 255.0, green: 204.0 / 255.0, blue: 66.0 / 255.0, alpha: 1.0)
+	static let white = UIColor(white: 245.0 / 255.0, alpha: 1.0)
+	static let whiteTwo = UIColor(white: 252.0 / 255.0, alpha: 1.0)
+	static let frogGreen = UIColor(red: 86.0 / 255.0, green: 179.0 / 255.0, blue: 11.0 / 255.0, alpha: 1.0)
+	static let softPink = UIColor(red: 249.0 / 255.0, green: 178.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+	static let hotPink = UIColor(red: 236.0 / 255.0, green: 0.0, blue: 140.0 / 255.0, alpha: 1.0)
+	static let veryLightPink = UIColor(white: 239.0 / 255.0, alpha: 1.0)
+}
