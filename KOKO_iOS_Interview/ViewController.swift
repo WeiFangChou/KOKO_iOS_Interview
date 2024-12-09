fe3d30be129c1cb180ef90df258776eb0ed86a70
WeiFangChou <chou.wei.fang@gmail.com>
2024-12-09 14:14:58 +0800

    Initial Commit

---
+++ KOKO_iOS_Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KOKO_iOS_Interview
+//
+//  Created by WeiFangChou on 2024/12/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
